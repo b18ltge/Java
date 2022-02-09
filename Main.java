d04ad463c2ea1ff949809fbd6c2c80b8746d64f9
b18ltge <87873850+b18ltge@users.noreply.github.com>
2022-02-09 14:35:20 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,47 +6,25 @@ class SolutionL1{
 	// #1
 	public static void Solve1() {
 		Scanner in = new Scanner(System.in);
-		System.out.println("--== Expression calculator ==--");
+		System.out.println("--== Завдання №1 ==--");
+		System.out.println("Обчислення значення числа e в залежності від значень чисел f та r");
 		System.out.println("e = 3f + 2r, f = 5");
 		System.out.println("e = √(8r) / (2f - 4), f = 0");
 		System.out.println("e = fr - 9, f = -5");
-		System.out.println("f is Integer, e is Decimal");
-		System.out.print("Enter f: ");
+		System.out.println("f є цілим числом, e - дійсним");
+		System.out.print("Введіть значення числа f: ");
 		byte f = in.nextByte();
-		System.out.print("Enter r: ");
+		System.out.print("Введіть значення числа r: ");
 		double r = in.nextDouble();
-		in.close();
 		
 		double e = .0;
-		switch(f) {
-			case 5:{
-				e = 15.0 + 2 * r;
-				break;
-			}
-			case 0:{
-				if (r < 0) {
-					System.out.print("Result: Unable to calculate expression √(8r) (r < 0)");
-					return;
-				}
-				e = Math.sqrt(8 * r) / -4.0;
-				break;
-			}
-			case -5:{
-				e = -5.0 * r - 9.0;
-				break;
-			}
-			default:{
-				System.out.print("Result: Unable to calculate expression");
-				return;
-			}
-		}
 		
-		/*if (f == 5) {
+		if (f == 5) {
 			e = 15.0 + 2 * r;
 		}
 		else if (f == 0) {
 			if (r < 0) {
-				System.out.print("Result: Unable to calculate expression √(8r) (r < 0)");
+				System.out.print("Результат: Неможливо обчислити значення √(8r), оскільки r < 0");
 				return;
 			}
 			e = Math.sqrt(8 * r) / -4.0;
@@ -55,10 +33,28 @@ class SolutionL1{
 			e = -5.0 * r - 9.0;
 		}
 		else {
-			System.out.print("Result: Unable to calculate expression");
+			System.out.print("Результат: Неможливо обчислити значення e (Число f не задовільняє ні одну умову)");
+		}
+		
+		/*if (f == 5) {
+			e = 15.0 + 2 * r;
+		}
+		if (f == 0) {
+			if (r < 0) {
+				System.out.print("Результат: Неможливо обчислити значення √(8r), оскільки r < 0");
+				return;
+			}
+			e = Math.sqrt(8 * r) / -4.0;
+		}
+		if (f == -5) {
+			e = -5.0 * r - 9.0;
+		}
+		if (f != 5 && f != 0 && f != -5) {
+			System.out.print("Результат: Неможливо обчислити значення e (Число f не задовільняє ні одну умову)");
+			return;
 		}*/
 		
-		System.out.printf("Result: e = %f",e);
+		System.out.printf("Результат: e = %f",e);
 	}
 
 	// #2
@@ -66,89 +62,150 @@ class SolutionL1{
 		double x1,y1,x2,y2,x3,y3;
 		Scanner in = new Scanner(System.in);
 		
-		System.out.println("\n--==Are 3 points on one straight line ? ==--");
-		System.out.print("Enter first point (x1,y1): ");
+		System.out.println(" --== Завдання №2 ==--");
+		System.out.println("Перевірка належності 3 точок одній прямій");
+		System.out.print("Введіть координати першої точки (x1 y1): ");
 		x1 = in.nextFloat();
 		y1 = in.nextFloat();
-		System.out.print("Enter second point (x2,y2): ");
+		System.out.print("Введіть координати другої точки (x2 y2): ");
 		x2 = in.nextFloat();
 		y2 = in.nextFloat();
-		System.out.print("Enter third point (x3,y3): ");
+		System.out.print("Введіть координати третьої точки (x3 y3): ");
 		x3 = in.nextFloat();
 		y3 = in.nextFloat();
-		in.close();
 		
 		double expr = (x3 - x1) / (x2 - x1) - (y3 - y1) / (y2 - y1);
 		if ( Math.pow(expr,2) <= 1e-8 ) {
-			System.out.println("3 points are on one straight line;");
+			System.out.println("Задані 3 точки лежать на одній прямій;");
 		}
 		else {
-			System.out.println("3 points aren't on one straight line;");
+			System.out.println("Задані 3 точки НЕ лежать на одній прямій;");
 		}
 		
 	}
 
 	// #3
 	public static void Solve3() {
-		System.out.println("--== Time: ==--");
-		System.out.print("Enter n (n >= 0 and n < 24): ");
+		System.out.println("--== Завдання №3 ==--");
+		System.out.print("Введіть порядковий номер години в добі: ");
 		Scanner in = new Scanner(System.in);
 		byte n = in.nextByte();
 		
-		if (n < 0 || n > 23) {
-			System.out.println("n > 23 or n < 0");
+		String str;
+		switch(n) {
+		case 1: str = "Перша";
+		break;
+		case 2: str = "Друга";
+		break;
+		case 3: str = "Третя";
+		break;
+		case 4: str = "Четверта";
+		break;
+		case 5: str = "П'ята";
+		break;
+		case 6: str = "Шоста";
+		break;
+		case 7: str = "Сьома";
+		break;
+		case 8: str = "Восьма";
+		break;
+		case 9: str = "Дев'ята";
+		break;
+		case 10: str = "Десята";
+		break;
+		case 11: str = "Одинадцята";
+		break;
+		case 12: str = "Дванадцята";
+		break;
+		case 13: str = "Тринадцята";
+		break;
+		case 14: str = "Чотирнадцята";
+		break;
+		case 15: str = "П'ятнадцята";
+		break;
+		case 16: str = "Шістнадцята";
+		break;
+		case 17: str = "Сімнадцята";
+		break;
+		case 18: str = "Вісімнадцята";
+		break;
+		case 19: str = "Дев'ятнадцята";
+		break;
+		case 20: str = "Двадцята";
+		break;
+		case 21: str = "Двадцять перша";
+		break;
+		case 22: str = "Двадцять друга";
+		break;
+		case 23: str = "Двадцять третя";
+		break;
+		case 24: str = "Двадцять четверта";
+		break;
+		default: {
+			System.out.println("Порядковий номер години в добі не повинен перевищувати 24 або бути меншим за 1;");
 			return;
 		}
-		
-		String str = " AM";
-		if (n == 0) {
-			n = 12;
 		}
-		else if (n == 12) {
-			str = " PM";
-		}
-		else if (n > 12) {
-			n %= 12;
-			str = " PM";
-		}
-		
-		System.out.println("It's " + n + str);
+		System.out.println(str + " година");
 		}
 
 	// #4
 	public static void Solve4() {
 		Scanner in = new Scanner(System.in);
-		System.out.println("--== Expression calculator: ==--");
-		System.out.println("e = √(3x / (z - 2y)) - yz + sin(x^2)");
-		System.out.print("Enter x:");
+		System.out.println("--== Завдання №4 ==--");
+		System.out.println("Обчислення значення функції t залежно від значень змінних x,y,z;");
+		System.out.println("t = √(3x / (z - 2y)) - yz + sin(x^2)");
+		System.out.print("Введіть значення змінної x:");
 		double x = in.nextDouble();
-		System.out.print("Enter y:");
+		System.out.print("Введіть значення змінної y:");
 		double y = in.nextDouble();
-		System.out.print("Enter z:");
+		System.out.print("Введіть значення змінної z:");
 		double z = in.nextDouble();
-		in.close();
 		
 		if (z - 2 * y == 0)
-			throw new ArithmeticException("z - 2y = 0"); 
+			throw new ArithmeticException("Для заданих значень y та z виконується ділення на 0;"); 
 		else if (3 * x / (z - 2 * y) < 0)
-			throw new ArithmeticException("3x / (z - 2y) < 0"); 
+			throw new ArithmeticException("Для заданих значень x, y та z підкореневий вираз < 0"); 
 		
 		double t = Math.sqrt(3 * x / (z - 2 * y)) - y * z + Math.sin(x * x);
-		System.out.println("Result: t = " + t);
+		System.out.println("Результат: t = " + t);
 	}
 }
 
 
 class Main {
 	public static void main(String[] args) {
-		//SolutionL1.Solve1();
-		//SolutionL1.Solve2();
-		//SolutionL1.Solve3();
-		try{
-			SolutionL1.Solve4();
+		byte n;
+		Scanner in = new Scanner(System.in);
+		while (true) {
+			do {
+			System.out.println("\nОберіть № завдання:");
+			System.out.println("1 - Завдання №1");
+			System.out.println("2 - Завдання №2");
+			System.out.println("3 - Завдання №3");
+			System.out.println("4 - Завдання №4");
+			System.out.println("0 - Вихід");
+			n = in.nextByte();
+		} while(n < 0 || n > 4);
+
+		switch(n) {
+		case 1: SolutionL1.Solve1();
+		break;
+		case 2: SolutionL1.Solve2();
+		break;
+		case 3: SolutionL1.Solve3();
+		break;
+		case 4: {
+			try{
+				SolutionL1.Solve4();
+			}
+			catch (Exception ex) {
+				System.out.println(ex.getMessage());
+			}
 		}
-		catch (Exception ex) {
-			System.out.println(ex.getMessage());
+		break;
+		default: return;
+		}
 		}
 	}
 }
