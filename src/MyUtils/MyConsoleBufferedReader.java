759c1c4e358ee95372589782540f46bdedbcae6a
b18ltge <87873850+b18ltge@users.noreply.github.com>
2022-04-20 13:37:51 +0300

    Delete src directory

--- src/MyUtils/MyConsoleBufferedReader.java
+++
@@ -1,8 +0,0 @@
-package MyUtils;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-public class MyConsoleBufferedReader {
-	public static BufferedReader buffReader = new BufferedReader(new InputStreamReader(System.in));
-}
