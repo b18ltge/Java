630d40010b53b8c03b1cdba64e47b5953cb32528
b18ltge <87873850+b18ltge@users.noreply.github.com>
2022-05-11 12:49:44 +0300

    Delete src directory

--- src/Misc/L8_2/Doctor.java
+++
@@ -1,8 +0,0 @@
-package Misc.L8_2;
-
-public abstract class Doctor implements IDoctor{
-	public static final String NAME_REGEX = "[a-zA-Z]{3,} [a-zA-Z]{3,} [a-zA-Z]{3,}";
-	
-	protected String name;
-	protected Type type;
-}
